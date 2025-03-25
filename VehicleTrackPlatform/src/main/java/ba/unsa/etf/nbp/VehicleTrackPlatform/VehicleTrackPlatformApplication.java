d9435be8f9f273abce3ed4a0c8096c04a85fb20d
Anida Nezovic <anezovic1@etf.unsa.ba>
2025-03-25 22:57:43 +0100

    Initial commit

---
+++ VehicleTrackPlatform/src/main/java/ba/unsa/etf/nbp/VehicleTrackPlatform/VehicleTrackPlatformApplication.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.nbp.VehicleTrackPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleTrackPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleTrackPlatformApplication.class, args);
+	}
+
+}
---
+++ VehicleTrackPlatform/src/test/java/ba/unsa/etf/nbp/VehicleTrackPlatform/VehicleTrackPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.nbp.VehicleTrackPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleTrackPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
