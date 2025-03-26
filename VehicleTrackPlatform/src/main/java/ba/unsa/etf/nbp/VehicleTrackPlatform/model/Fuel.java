741a353644ee34f3a781a42ed364564435636166
amehanovic2 <amehanovic2@etf.unsa.ba>
2025-03-26 15:07:13 +0100

    Add Fuel model

---
+++ VehicleTrackPlatform/src/main/java/ba/unsa/etf/nbp/VehicleTrackPlatform/model/Fuel.java
@@ -0,0 +1,30 @@
+package ba.unsa.etf.nbp.VehicleTrackPlatform.model;
+
+public class Fuel {
+    private Long id;
+    private String name;
+
+    public Fuel() {
+    }
+
+    public Fuel(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
