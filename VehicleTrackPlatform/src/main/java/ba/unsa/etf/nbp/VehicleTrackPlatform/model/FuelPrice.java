b21c4fa1f89d56d6f9053d2115a1cf1d540ceab6
amehanovic2 <amehanovic2@etf.unsa.ba>
2025-03-26 15:08:57 +0100

    Add FuelPrice model

---
+++ VehicleTrackPlatform/src/main/java/ba/unsa/etf/nbp/VehicleTrackPlatform/model/FuelPrice.java
@@ -0,0 +1,42 @@
+package ba.unsa.etf.nbp.VehicleTrackPlatform.model;
+
+import java.time.LocalDate;
+
+public class FuelPrice {
+    private Long id;
+    private LocalDate issueDate;
+    private Long fuelId;
+
+    public FuelPrice() {
+    }
+
+    public FuelPrice(Long id, LocalDate issueDate, Long fuelId) {
+        this.id = id;
+        this.issueDate = issueDate;
+        this.fuelId = fuelId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getIssueDate() {
+        return issueDate;
+    }
+
+    public void setIssueDate(LocalDate issueDate) {
+        this.issueDate = issueDate;
+    }
+
+    public Long getFuelId() {
+        return fuelId;
+    }
+
+    public void setFuelId(Long fuelId) {
+        this.fuelId = fuelId;
+    }
+}
