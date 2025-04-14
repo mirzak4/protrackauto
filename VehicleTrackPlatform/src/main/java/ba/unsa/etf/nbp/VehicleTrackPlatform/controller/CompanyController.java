5c31a34feb17b6082209c7bfa47a2b99536a4b01
Mirza Kadric <mirza.kadric@authoritypartners.com>
2025-04-14 12:47:38 +0200

    add api response summary on company controller

--- VehicleTrackPlatform/src/main/java/ba/unsa/etf/nbp/VehicleTrackPlatform/controller/CompanyController.java
+++ VehicleTrackPlatform/src/main/java/ba/unsa/etf/nbp/VehicleTrackPlatform/controller/CompanyController.java
@@ -3,6 +3,10 @@ package ba.unsa.etf.nbp.VehicleTrackPlatform.controller;
 import ba.unsa.etf.nbp.VehicleTrackPlatform.dto.CompanyDTO;
 import ba.unsa.etf.nbp.VehicleTrackPlatform.model.enums.CompanyType;
 import ba.unsa.etf.nbp.VehicleTrackPlatform.service.CompanyService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,11 +25,22 @@ public class CompanyController {
         this.companyService = companyService;
     }
 
+    @Operation(summary = "Get all companies by type", description = "Returns all companies with the provided type")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of companies"),
+            @ApiResponse(responseCode = "400", description = "Invalid company type supplied", content = @Content),
+    })
     @GetMapping
     public List<CompanyDTO> getAllCompanies(@RequestParam CompanyType companyType) {
         return companyService.getAllCompanies(companyType);
     }
 
+    @Operation(summary = "Get company by id", description = "Returns the company if it exists")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Company found"),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Company not found", content = @Content)
+    })
     @GetMapping("/{id}")
     public ResponseEntity<CompanyDTO> getCompanyById(@PathVariable Long id) {
         return companyService.getCompanyById(id)
@@ -33,6 +48,11 @@ public class CompanyController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Create a new company", description = "Returns the id of newly created company")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Company created"),
+            @ApiResponse(responseCode = "400", description = "Invalid company payload supplied", content = @Content)
+    })
     @PostMapping
     public ResponseEntity<Long> createCompany(@RequestBody CompanyDTO companyDTO) {
         Long companyId = companyService.createCompany(companyDTO);
@@ -40,12 +60,24 @@ public class CompanyController {
                 .body(companyId);
     }
 
+    @Operation(summary = "Update existing company", description = "Returns the updated company")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Company updated successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid company payload supplied", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Company not found", content = @Content)
+    })
     @PutMapping("/{id}")
     public ResponseEntity<CompanyDTO> updateCompany(@PathVariable Long id, @RequestBody CompanyDTO companyDTO) {
         companyDTO.setId(id);
         return ResponseEntity.ok(companyService.updateCompany(companyDTO));
     }
 
+    @Operation(summary = "Delete company by id", description = "Deletes company with provided id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Company deleted successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Company not found", content = @Content)
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteCompany(@PathVariable Long id) {
         companyService.deleteCompany(id);
