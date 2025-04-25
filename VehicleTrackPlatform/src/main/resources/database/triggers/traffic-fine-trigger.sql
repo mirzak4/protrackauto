074fee9e709d979c14b0bd4d582a397779a73d13
amehanovic2 <amehanovic2@etf.unsa.ba>
2025-04-25 17:00:59 +0200

    Add triggers for traffic fine and travel request

---
+++ VehicleTrackPlatform/src/main/resources/database/triggers/traffic-fine-trigger.sql
@@ -0,0 +1,31 @@
+CREATE OR REPLACE TRIGGER trg_traffic_fine_audit
+    AFTER INSERT OR UPDATE OR DELETE ON traffic_fine
+    FOR EACH ROW
+DECLARE
+    v_action VARCHAR2(10);
+    v_user VARCHAR2(100);
+BEGIN
+    IF INSERTING THEN
+        v_action := 'INSERT';
+        v_user := COALESCE(:NEW.modified_by, USER);
+    ELSIF UPDATING THEN
+        v_action := 'UPDATE';
+        v_user := COALESCE(:NEW.modified_by, USER);
+    ELSIF DELETING THEN
+        v_action := 'DELETE';
+        v_user := COALESCE(:OLD.modified_by, USER);
+    END IF;
+
+    INSERT INTO nbp.nbp_log (
+        action_name,
+        table_name,
+        date_time,
+        db_user
+    )
+    VALUES (
+               v_action,
+               'TRAFFIC_FINE',
+               SYSTIMESTAMP,
+               v_user
+           );
+END;
