1f0eeabb4070ca9d5e8885baf6a77a12ed083a2f
Mirza Kadric <mirza.kadric@authoritypartners.com>
2025-05-14 22:10:06 +0200

    hash default user passwords

--- VehicleTrackPlatform/src/main/resources/database/scripts/driver-seed.sql
+++ VehicleTrackPlatform/src/main/resources/database/scripts/driver-seed.sql
@@ -17,7 +17,7 @@ BEGIN
                         --DECLARE ANIDA_USER_ID INTEGER;
                         --BEGIN
                         --    INSERT INTO nbp.nbp_user (FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, USERNAME, PHONE_NUMBER, BIRTH_DATE, ROLE_ID)
-                        --    VALUES ('Anida', 'Nezovic', 'anezovic@nbp.com', 'pass_123', 'anezovic', '111-333', TO_DATE('2000-04-20', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
+                        --    VALUES ('Anida', 'Nezovic', 'anezovic@nbp.com', '$2a$12$JhweS6xXjWMgmTcVcUDcpuPA4K8jKWCq651hrQvfxsS6/MuzJOaVq', 'anezovic', '111-333', TO_DATE('2000-04-20', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
                         --    RETURNING ID INTO ANIDA_USER_ID;
                         --    INSERT INTO NBP04.DRIVER (USER_ID, LICENSE_NUMBER, LICENSE_EXPIRY)
                         --    VALUES (ANIDA_USER_ID, '123-456-789', TO_TIMESTAMP('2035-04-20 15:45:30', 'YYYY-MM-DD HH24:MI:SS'));
@@ -26,7 +26,7 @@ BEGIN
                         --DECLARE NADA_USER_ID INTEGER;
                         --BEGIN
                         --    INSERT INTO nbp.nbp_user (FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, USERNAME, PHONE_NUMBER, BIRTH_DATE, ROLE_ID)
-                        --    VALUES ('Nada', 'Maric', 'nmaric@nbp.com', 'pass_123', 'nmaric', '111-333', TO_DATE('2000-04-20', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
+                        --    VALUES ('Nada', 'Maric', 'nmaric@nbp.com', '$2a$12$JhweS6xXjWMgmTcVcUDcpuPA4K8jKWCq651hrQvfxsS6/MuzJOaVq', 'nmaric', '111-333', TO_DATE('2000-04-20', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
                         --    RETURNING ID INTO NADA_USER_ID;
                         --    INSERT INTO NBP04.DRIVER (USER_ID, LICENSE_NUMBER, LICENSE_EXPIRY)
                         --    VALUES (NADA_USER_ID, '5A4-4H6-8P9', TO_TIMESTAMP('2035-04-20 15:45:30', 'YYYY-MM-DD HH24:MI:SS'));
@@ -35,7 +35,7 @@ BEGIN
                         DECLARE LANA_USER_ID INTEGER;
                         BEGIN
                             INSERT INTO nbp.nbp_user (FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, USERNAME, PHONE_NUMBER, BIRTH_DATE, ROLE_ID)
-                            VALUES ('Lana', 'Kovac', 'lkovac@nbp.com', 'pass_123', 'lkovac', '111-335', TO_DATE('1992-11-05', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
+                            VALUES ('Lana', 'Kovac', 'lkovac@nbp.com', '$2a$12$JhweS6xXjWMgmTcVcUDcpuPA4K8jKWCq651hrQvfxsS6/MuzJOaVq', 'lkovac', '111-335', TO_DATE('1992-11-05', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
                             RETURNING ID INTO LANA_USER_ID;
                             INSERT INTO NBP04.DRIVER (USER_ID, LICENSE_NUMBER, LICENSE_EXPIRY)
                             VALUES (LANA_USER_ID, '8H9-2J3-4L5', TO_TIMESTAMP('2036-11-05 09:30:00', 'YYYY-MM-DD HH24:MI:SS'));
@@ -44,7 +44,7 @@ BEGIN
                         DECLARE MARKO_USER_ID INTEGER;
                         BEGIN
                             INSERT INTO nbp.nbp_user (FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, USERNAME, PHONE_NUMBER, BIRTH_DATE, ROLE_ID)
-                            VALUES ('Marko', 'Petrovic', 'mpetrovic@nbp.com', 'pass_123', 'mpetrovic', '111-336', TO_DATE('1990-02-20', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
+                            VALUES ('Marko', 'Petrovic', 'mpetrovic@nbp.com', '$2a$12$JhweS6xXjWMgmTcVcUDcpuPA4K8jKWCq651hrQvfxsS6/MuzJOaVq', 'mpetrovic', '111-336', TO_DATE('1990-02-20', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
                             RETURNING ID INTO MARKO_USER_ID;
                             INSERT INTO NBP04.DRIVER (USER_ID, LICENSE_NUMBER, LICENSE_EXPIRY)
                             VALUES (MARKO_USER_ID, '9K1-5M2-7N3', TO_TIMESTAMP('2035-02-20 14:15:00', 'YYYY-MM-DD HH24:MI:SS'));
@@ -53,7 +53,7 @@ BEGIN
                         DECLARE LEJLA_USER_ID INTEGER;
                         BEGIN
                             INSERT INTO nbp.nbp_user (FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, USERNAME, PHONE_NUMBER, BIRTH_DATE, ROLE_ID)
-                            VALUES ('Lejla', 'Mustafic', 'lmustafic@nbp.com', 'pass_123', 'lmustafic', '111-339', TO_DATE('1993-03-10', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
+                            VALUES ('Lejla', 'Mustafic', 'lmustafic@nbp.com', '$2a$12$JhweS6xXjWMgmTcVcUDcpuPA4K8jKWCq651hrQvfxsS6/MuzJOaVq', 'lmustafic', '111-339', TO_DATE('1993-03-10', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
                             RETURNING ID INTO LEJLA_USER_ID;
                             INSERT INTO NBP04.DRIVER (USER_ID, LICENSE_NUMBER, LICENSE_EXPIRY)
                             VALUES (LEJLA_USER_ID, '4M2-5N7-8O6', TO_TIMESTAMP('2036-03-10 12:00:00', 'YYYY-MM-DD HH24:MI:SS'));
@@ -62,7 +62,7 @@ BEGIN
                         DECLARE ADNAN_USER_ID INTEGER;
                         BEGIN
                             INSERT INTO nbp.nbp_user (FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, USERNAME, PHONE_NUMBER, BIRTH_DATE, ROLE_ID)
-                            VALUES ('Adnan', 'Kurtovic', 'akurtovic@nbp.com', 'pass_123', 'akurtovic', '111-340', TO_DATE('1991-09-25', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
+                            VALUES ('Adnan', 'Kurtovic', 'akurtovic@nbp.com', '$2a$12$JhweS6xXjWMgmTcVcUDcpuPA4K8jKWCq651hrQvfxsS6/MuzJOaVq', 'akurtovic', '111-340', TO_DATE('1991-09-25', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
                             RETURNING ID INTO ADNAN_USER_ID;
                             INSERT INTO NBP04.DRIVER (USER_ID, LICENSE_NUMBER, LICENSE_EXPIRY)
                             VALUES (ADNAN_USER_ID, '5P1-9Q3-2R8', TO_TIMESTAMP('2035-09-25 16:30:00', 'YYYY-MM-DD HH24:MI:SS'));
@@ -71,7 +71,7 @@ BEGIN
                         DECLARE MERJEM_USER_ID INTEGER;
                         BEGIN
                             INSERT INTO nbp.nbp_user (FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, USERNAME, PHONE_NUMBER, BIRTH_DATE, ROLE_ID)
-                            VALUES ('Merjem', 'Softic', 'msoftic@nbp.com', 'pass_123', 'msoftic', '111-341', TO_DATE('1994-07-22', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
+                            VALUES ('Merjem', 'Softic', 'msoftic@nbp.com', '$2a$12$JhweS6xXjWMgmTcVcUDcpuPA4K8jKWCq651hrQvfxsS6/MuzJOaVq', 'msoftic', '111-341', TO_DATE('1994-07-22', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
                             RETURNING ID INTO MERJEM_USER_ID;
                             INSERT INTO NBP04.DRIVER (USER_ID, LICENSE_NUMBER, LICENSE_EXPIRY)
                             VALUES (MERJEM_USER_ID, '6S3-4T5-7U8', TO_TIMESTAMP('2036-07-22 13:15:00', 'YYYY-MM-DD HH24:MI:SS'));
@@ -80,7 +80,7 @@ BEGIN
                         DECLARE NERMIN_USER_ID INTEGER;
                         BEGIN
                             INSERT INTO nbp.nbp_user (FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, USERNAME, PHONE_NUMBER, BIRTH_DATE, ROLE_ID)
-                            VALUES ('Nermin', 'Begovic', 'nbegovic@nbp.com', 'pass_123', 'nbegovic', '111-342', TO_DATE('1996-01-30', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
+                            VALUES ('Nermin', 'Begovic', 'nbegovic@nbp.com', '$2a$12$JhweS6xXjWMgmTcVcUDcpuPA4K8jKWCq651hrQvfxsS6/MuzJOaVq', 'nbegovic', '111-342', TO_DATE('1996-01-30', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
                             RETURNING ID INTO NERMIN_USER_ID;
                             INSERT INTO NBP04.DRIVER (USER_ID, LICENSE_NUMBER, LICENSE_EXPIRY)
                             VALUES (NERMIN_USER_ID, '7V6-8W9-1X2', TO_TIMESTAMP('2035-01-30 09:00:00', 'YYYY-MM-DD HH24:MI:SS'));
@@ -89,7 +89,7 @@ BEGIN
                         DECLARE DINO_USER_ID INTEGER;
                         BEGIN
                             INSERT INTO nbp.nbp_user (FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, USERNAME, PHONE_NUMBER, BIRTH_DATE, ROLE_ID)
-                            VALUES ('Dino', 'Radic', 'dradic@nbp.com', 'pass_123', 'dradic', '111-343', TO_DATE('1993-05-14', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
+                            VALUES ('Dino', 'Radic', 'dradic@nbp.com', '$2a$12$JhweS6xXjWMgmTcVcUDcpuPA4K8jKWCq651hrQvfxsS6/MuzJOaVq', 'dradic', '111-343', TO_DATE('1993-05-14', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
                             RETURNING ID INTO DINO_USER_ID;
                             INSERT INTO NBP04.DRIVER (USER_ID, LICENSE_NUMBER, LICENSE_EXPIRY)
                             VALUES (DINO_USER_ID, '8Y2-3Z4-5A6', TO_TIMESTAMP('2037-05-14 11:45:00', 'YYYY-MM-DD HH24:MI:SS'));
@@ -98,7 +98,7 @@ BEGIN
                         DECLARE INES_USER_ID INTEGER;
                         BEGIN
                             INSERT INTO nbp.nbp_user (FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, USERNAME, PHONE_NUMBER, BIRTH_DATE, ROLE_ID)
-                            VALUES ('Ines', 'Karacic', 'ikaracic@nbp.com', 'pass_123', 'ikaracic', '111-344', TO_DATE('1992-10-02', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
+                            VALUES ('Ines', 'Karacic', 'ikaracic@nbp.com', '$2a$12$JhweS6xXjWMgmTcVcUDcpuPA4K8jKWCq651hrQvfxsS6/MuzJOaVq', 'ikaracic', '111-344', TO_DATE('1992-10-02', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
                             RETURNING ID INTO INES_USER_ID;
                             INSERT INTO NBP04.DRIVER (USER_ID, LICENSE_NUMBER, LICENSE_EXPIRY)
                             VALUES (INES_USER_ID, '9B7-0C1-2D3', TO_TIMESTAMP('2036-10-02 10:30:00', 'YYYY-MM-DD HH24:MI:SS'));
@@ -107,7 +107,7 @@ BEGIN
                         --DECLARE JOHN_USER_ID INTEGER;
                         --BEGIN
                         --    INSERT INTO nbp.nbp_user (FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, USERNAME, PHONE_NUMBER, BIRTH_DATE, ROLE_ID)
-                        --    VALUES ('John', 'Doe', 'jdoe@nbp.com', 'pass_123', 'jdoe', '111-345', TO_DATE('1985-01-15', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
+                        --    VALUES ('John', 'Doe', 'jdoe@nbp.com', '$2a$12$JhweS6xXjWMgmTcVcUDcpuPA4K8jKWCq651hrQvfxsS6/MuzJOaVq', 'jdoe', '111-345', TO_DATE('1985-01-15', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
                         --    RETURNING ID INTO JOHN_USER_ID;
                         --    INSERT INTO NBP04.DRIVER (USER_ID, LICENSE_NUMBER, LICENSE_EXPIRY)
                         --    VALUES (JOHN_USER_ID, 'J1D-2E3-4F5', TO_TIMESTAMP('2035-01-15 10:00:00', 'YYYY-MM-DD HH24:MI:SS'));
@@ -116,7 +116,7 @@ BEGIN
                         DECLARE JANE_USER_ID INTEGER;
                         BEGIN
                             INSERT INTO nbp.nbp_user (FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, USERNAME, PHONE_NUMBER, BIRTH_DATE, ROLE_ID)
-                            VALUES ('Jane', 'Smith', 'jsmith@nbp.com', 'pass_123', 'jsmith', '111-346', TO_DATE('1990-05-22', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
+                            VALUES ('Jane', 'Smith', 'jsmith@nbp.com', '$2a$12$JhweS6xXjWMgmTcVcUDcpuPA4K8jKWCq651hrQvfxsS6/MuzJOaVq', 'jsmith', '111-346', TO_DATE('1990-05-22', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
                             RETURNING ID INTO JANE_USER_ID;
                             INSERT INTO NBP04.DRIVER (USER_ID, LICENSE_NUMBER, LICENSE_EXPIRY)
                             VALUES (JANE_USER_ID, 'J2S-3M4-5N6', TO_TIMESTAMP('2036-05-22 09:30:00', 'YYYY-MM-DD HH24:MI:SS'));
@@ -125,7 +125,7 @@ BEGIN
                         DECLARE MICHAEL_USER_ID INTEGER;
                         BEGIN
                             INSERT INTO nbp.nbp_user (FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, USERNAME, PHONE_NUMBER, BIRTH_DATE, ROLE_ID)
-                            VALUES ('Michael', 'Brown', 'mbrown@nbp.com', 'pass_123', 'mbrown', '111-347', TO_DATE('1988-03-10', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
+                            VALUES ('Michael', 'Brown', 'mbrown@nbp.com', '$2a$12$JhweS6xXjWMgmTcVcUDcpuPA4K8jKWCq651hrQvfxsS6/MuzJOaVq', 'mbrown', '111-347', TO_DATE('1988-03-10', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
                             RETURNING ID INTO MICHAEL_USER_ID;
                             INSERT INTO NBP04.DRIVER (USER_ID, LICENSE_NUMBER, LICENSE_EXPIRY)
                             VALUES (MICHAEL_USER_ID, 'M3B-4R5-6O7', TO_TIMESTAMP('2035-03-10 11:45:00', 'YYYY-MM-DD HH24:MI:SS'));
@@ -134,7 +134,7 @@ BEGIN
                         DECLARE EMILY_USER_ID INTEGER;
                         BEGIN
                             INSERT INTO nbp.nbp_user (FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, USERNAME, PHONE_NUMBER, BIRTH_DATE, ROLE_ID)
-                            VALUES ('Emily', 'Johnson', 'ejohnson@nbp.com', 'pass_123', 'ejohnson', '111-348', TO_DATE('1992-07-18', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
+                            VALUES ('Emily', 'Johnson', 'ejohnson@nbp.com', '$2a$12$JhweS6xXjWMgmTcVcUDcpuPA4K8jKWCq651hrQvfxsS6/MuzJOaVq', 'ejohnson', '111-348', TO_DATE('1992-07-18', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
                             RETURNING ID INTO EMILY_USER_ID;
                             INSERT INTO NBP04.DRIVER (USER_ID, LICENSE_NUMBER, LICENSE_EXPIRY)
                             VALUES (EMILY_USER_ID, 'E4J-5H6-7P8', TO_TIMESTAMP('2036-07-18 08:15:00', 'YYYY-MM-DD HH24:MI:SS'));
@@ -143,7 +143,7 @@ BEGIN
                         DECLARE DAVID_USER_ID INTEGER;
                         BEGIN
                             INSERT INTO nbp.nbp_user (FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, USERNAME, PHONE_NUMBER, BIRTH_DATE, ROLE_ID)
-                            VALUES ('David', 'Wilson', 'dwilson@nbp.com', 'pass_123', 'dwilson', '111-349', TO_DATE('1987-11-30', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
+                            VALUES ('David', 'Wilson', 'dwilson@nbp.com', '$2a$12$JhweS6xXjWMgmTcVcUDcpuPA4K8jKWCq651hrQvfxsS6/MuzJOaVq', 'dwilson', '111-349', TO_DATE('1987-11-30', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
                             RETURNING ID INTO DAVID_USER_ID;
                             INSERT INTO NBP04.DRIVER (USER_ID, LICENSE_NUMBER, LICENSE_EXPIRY)
                             VALUES (DAVID_USER_ID, 'D5W-6I7-8Q9', TO_TIMESTAMP('2035-11-30 14:00:00', 'YYYY-MM-DD HH24:MI:SS'));
@@ -152,7 +152,7 @@ BEGIN
                         DECLARE LAURA_USER_ID INTEGER;
                         BEGIN
                             INSERT INTO nbp.nbp_user (FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, USERNAME, PHONE_NUMBER, BIRTH_DATE, ROLE_ID)
-                            VALUES ('Laura', 'Miller', 'lmiller@nbp.com', 'pass_123', 'lmiller', '111-350', TO_DATE('1991-02-25', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
+                            VALUES ('Laura', 'Miller', 'lmiller@nbp.com', '$2a$12$JhweS6xXjWMgmTcVcUDcpuPA4K8jKWCq651hrQvfxsS6/MuzJOaVq', 'lmiller', '111-350', TO_DATE('1991-02-25', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
                             RETURNING ID INTO LAURA_USER_ID;
                             INSERT INTO NBP04.DRIVER (USER_ID, LICENSE_NUMBER, LICENSE_EXPIRY)
                             VALUES (LAURA_USER_ID, 'L6M-7E8-9R0', TO_TIMESTAMP('2036-02-25 12:30:00', 'YYYY-MM-DD HH24:MI:SS'));
@@ -161,7 +161,7 @@ BEGIN
                         DECLARE DANIEL_USER_ID INTEGER;
                         BEGIN
                             INSERT INTO nbp.nbp_user (FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, USERNAME, PHONE_NUMBER, BIRTH_DATE, ROLE_ID)
-                            VALUES ('Daniel', 'Anderson', 'danderson@nbp.com', 'pass_123', 'danderson', '111-351', TO_DATE('1989-09-05', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
+                            VALUES ('Daniel', 'Anderson', 'danderson@nbp.com', '$2a$12$JhweS6xXjWMgmTcVcUDcpuPA4K8jKWCq651hrQvfxsS6/MuzJOaVq', 'danderson', '111-351', TO_DATE('1989-09-05', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
                             RETURNING ID INTO DANIEL_USER_ID;
                             INSERT INTO NBP04.DRIVER (USER_ID, LICENSE_NUMBER, LICENSE_EXPIRY)
                             VALUES (DANIEL_USER_ID, 'D7A-8N9-0S1', TO_TIMESTAMP('2035-09-05 16:45:00', 'YYYY-MM-DD HH24:MI:SS'));
@@ -170,7 +170,7 @@ BEGIN
                         DECLARE OLIVIA_USER_ID INTEGER;
                         BEGIN
                             INSERT INTO nbp.nbp_user (FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, USERNAME, PHONE_NUMBER, BIRTH_DATE, ROLE_ID)
-                            VALUES ('Olivia', 'Thomas', 'othomas@nbp.com', 'pass_123', 'othomas', '111-352', TO_DATE('1993-12-12', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
+                            VALUES ('Olivia', 'Thomas', 'othomas@nbp.com', '$2a$12$JhweS6xXjWMgmTcVcUDcpuPA4K8jKWCq651hrQvfxsS6/MuzJOaVq', 'othomas', '111-352', TO_DATE('1993-12-12', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
                             RETURNING ID INTO OLIVIA_USER_ID;
                             INSERT INTO NBP04.DRIVER (USER_ID, LICENSE_NUMBER, LICENSE_EXPIRY)
                             VALUES (OLIVIA_USER_ID, 'O8T-9H0-1U2', TO_TIMESTAMP('2036-12-12 10:15:00', 'YYYY-MM-DD HH24:MI:SS'));
@@ -179,7 +179,7 @@ BEGIN
                         DECLARE JAMES_USER_ID INTEGER;
                         BEGIN
                             INSERT INTO nbp.nbp_user (FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, USERNAME, PHONE_NUMBER, BIRTH_DATE, ROLE_ID)
-                            VALUES ('James', 'Taylor', 'jtaylor@nbp.com', 'pass_123', 'jtaylor', '111-353', TO_DATE('1986-04-03', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
+                            VALUES ('James', 'Taylor', 'jtaylor@nbp.com', '$2a$12$JhweS6xXjWMgmTcVcUDcpuPA4K8jKWCq651hrQvfxsS6/MuzJOaVq', 'jtaylor', '111-353', TO_DATE('1986-04-03', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
                             RETURNING ID INTO JAMES_USER_ID;
                             INSERT INTO NBP04.DRIVER (USER_ID, LICENSE_NUMBER, LICENSE_EXPIRY)
                             VALUES (JAMES_USER_ID, 'J9T-0A1-2V3', TO_TIMESTAMP('2035-04-03 13:15:00', 'YYYY-MM-DD HH24:MI:SS'));
@@ -188,7 +188,7 @@ BEGIN
                         DECLARE SOPHIA_USER_ID INTEGER;
                         BEGIN
                             INSERT INTO nbp.nbp_user (FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, USERNAME, PHONE_NUMBER, BIRTH_DATE, ROLE_ID)
-                            VALUES ('Sophia', 'Moore', 'smoore@nbp.com', 'pass_123', 'smoore', '111-354', TO_DATE('1994-08-27', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
+                            VALUES ('Sophia', 'Moore', 'smoore@nbp.com', '$2a$12$JhweS6xXjWMgmTcVcUDcpuPA4K8jKWCq651hrQvfxsS6/MuzJOaVq', 'smoore', '111-354', TO_DATE('1994-08-27', 'YYYY-MM-DD'), DRIVER_ROLE_ID)
                             RETURNING ID INTO SOPHIA_USER_ID;
                             INSERT INTO NBP04.DRIVER (USER_ID, LICENSE_NUMBER, LICENSE_EXPIRY)
                             VALUES (SOPHIA_USER_ID, 'S0M-1O2-3W4', TO_TIMESTAMP('2036-08-27 09:45:00', 'YYYY-MM-DD HH24:MI:SS'));
