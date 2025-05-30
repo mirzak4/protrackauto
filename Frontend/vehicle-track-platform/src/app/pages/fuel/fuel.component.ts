cb63f87e84db58d8a7e4100b9d0910865aa518c7
apandzic2 <apandzic2@etf.unsa.ba>
2025-05-30 16:44:26 +0200

    Added

--- Frontend/vehicle-track-platform/src/app/pages/fuel/fuel.component.ts
+++ Frontend/vehicle-track-platform/src/app/pages/fuel/fuel.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-fuel',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './fuel.component.html',
-  styleUrl: './fuel.component.css'
+  styleUrls: ['./fuel.component.css']
 })
 export class FuelComponent {
+  fuelRecords = [
+    { date: '2025-01-01', vehicle: 'Truck A', volume: 50, cost: 120.5 },
+    { date: '2025-01-05', vehicle: 'Van B', volume: 30, cost: 75.2 },
+    { date: '2025-01-12', vehicle: 'Truck C', volume: 40, cost: 98.7 }
+  ];
 
+  getTotalCost(): number {
+    return this.fuelRecords.reduce((sum, record) => sum + record.cost, 0);
+  }
 }
