0d67d1601d2d689c134cc28164b96faac2079240
apandzic2 <apandzic2@etf.unsa.ba>
2025-06-09 16:46:16 +0200

    Added traffic fines page

--- Frontend/vehicle-track-platform/src/app/app.routes.ts
+++ Frontend/vehicle-track-platform/src/app/app.routes.ts
@@ -5,7 +5,6 @@ import { DriverFormComponent } from './pages/drivers/driver-form/driver-form.com
 import { ServicesComponent } from './pages/services/services.component';
 import { FuelComponent } from './pages/fuel/fuel.component';
 import { InsuranceComponent } from './pages/insurance/insurance.component';
-import { FinesComponent } from './pages/fines/fines.component';
 import { InspectionsComponent } from './pages/inspections/inspections.component';
 import { TravelRequestsComponent } from './pages/travel-requests/travel-requests.component';
 import { AccountComponent } from './pages/account/account.component';
@@ -22,6 +21,8 @@ import { EmployeeListComponent } from './pages/employee/employee-list/employee-l
 import { ServiceRequestFormComponent } from './pages/service-request/service-request-form/service-request-form.component';
 import { ServiceRequestListComponent } from './pages/service-request/service-request-list/service-request-list.component';
 import { FuelPriceManagement } from './pages/fuel-price-management/fuel-price-management.component';
+import { TrafficFineFormComponent } from './pages/traffic-fines/traffic-fine-form/traffic-fine-form.component';
+import { TrafficFineListComponent } from './pages/traffic-fines/traffic-fine-list/traffic-fine-list.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -46,11 +47,14 @@ export const routes: Routes = [
   { path: 'service-requests', component: ServiceRequestListComponent },
   { path: 'service-requests/new', component: ServiceRequestFormComponent },
   { path: 'service-requests/edit/:id', component: ServiceRequestFormComponent },
+  
+  { path: 'traffic-fines', component: TrafficFineListComponent },
+  { path: 'traffic-fines/new', component: TrafficFineFormComponent },
+  { path: 'traffic-fines/edit/:id', component: TrafficFineFormComponent },
 
   { path: 'services', component: ServicesComponent },
   { path: 'fuel', component: FuelComponent },
   { path: 'insurance', component: InsuranceComponent },
-  { path: 'fines', component: FinesComponent },
   { path: 'inspections', component: InspectionsComponent },
   { path: 'travel-requests', component: TravelRequestsComponent },
   { path: 'account', component: AccountComponent },
